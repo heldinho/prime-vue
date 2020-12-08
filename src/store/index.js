b0480e4f9acc590eb6caa3a687462292f15c0604
heldinho <helderferreirapassos@gmail.com>
2020-12-08 00:08:49 -0200

    init

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,28 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue'
+import Vuex from 'vuex'
+import VuexPersist from 'vuex-persist'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
+
+const vuexLocalStorage = new VuexPersist({
+  key: 'vuex',
+  storage: window.localStorage
+})
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
-});
+  state: {
+    fullname: ''
+  },
+  mutations: {
+    SET_FULLNAME: function(state, name) {
+      state.fullname = name
+    }
+  },
+  actions: {
+    actionFullname: function({ commit }, name) {
+      commit('SET_FULLNAME', name)
+    }
+  },
+  modules: {},
+  plugins: [vuexLocalStorage.plugin]
+})
